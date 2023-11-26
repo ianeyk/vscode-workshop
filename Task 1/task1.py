f60ee30c2cfca83df7d159ec0c3cadd9462c4146
Ian Eykamp <ian@eykamp.com>
2023-11-26 16:15:53 -0500

    start task 1

---
+++ Task 1/task1.py
@@ -0,0 +1,9 @@
+# Task 1: Make this look pretty!
+
+def hello_world(x):
+    for i in range(x):
+        print("Hello, World!")
+        print(i)
+
+def math_expression(a, b, c):
+    return (c + ((a + b) ** ((a - (b + c)) * c)) ** b)
